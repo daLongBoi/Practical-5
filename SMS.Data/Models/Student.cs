4d365a8a3f9cf0103f6993dcfaea873195f2ae2d
daLongBoi <duncan.kane1997@hotmail.com>
2022-02-23 14:31:41 +0000

    completed

--- SMS.Data/Models/Student.cs
+++ SMS.Data/Models/Student.cs
@@ -5,18 +5,22 @@ namespace SMS.Data.Models
 {
     public class Student
     {
+        [Required]
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
-
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Required]
         public string Course { get; set; }
-
+        [Required]
+        [Range (12,90)]
         public int Age { get; set; } 
-
+        [Required]
+        [Range (0,100)]
         public double Grade { get; set; }
-
+        [Url]
         public string PhotoUrl { get; set; }     
 
         // Read-Only property - not stored in database
